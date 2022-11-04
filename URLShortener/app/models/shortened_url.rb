48bcdc5717d29c6dbe5025be36b3628f9e4aa9f2
Patrick Choi <ptrckchoi@gmail.com>
2022-11-03 17:55:22 -0700

    finished random_code generator

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -3,6 +3,25 @@ class ShortenedUrl < ApplicationRecord
   validates :short_url, presence: true, uniqueness: true
   validates :user_id, presence: true
 
-  
+  belongs_to :user,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :User
+
+
+  # after_initialize do |user|
+  #   #calls generate_short_url
+
+  #   ShortenedUrl.random_code
+  # end
+
+  def self.random_code
+    temp=false
+    while !temp
+      code = SecureRandom.urlsafe_base64
+      temp=true if !ShortenedUrl.exists?(:short_url => code)
+    end
+    code
+  end
 
 end
