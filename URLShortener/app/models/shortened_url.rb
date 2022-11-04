b7c0cf87bc8e0bd28908541ae66f8651b89521e0
remnantsofred <remnantsofred@gmail.com>
2022-11-03 17:10:04 -0700

    Create shortened_url project

---
+++ URLShortener/app/models/shortened_url.rb
@@ -0,0 +1,8 @@
+class ShortenedUrl < ApplicationRecord
+  validates :long_url, presence: true, uniqueness: true
+  validates :short_url, presence: true, uniqueness: true
+  validates :user_id, presence: true
+
+  
+
+end
---
+++ URLShortener/db/migrate/20221103235008_create_shortened_urls.rb
@@ -0,0 +1,10 @@
+class CreateShortenedUrls < ActiveRecord::Migration[7.0]
+  def change
+    create_table :shortened_urls do |t|
+      t.string :long_url, null: false, index: {unique: true}
+      t.string :short_url, null: false, index: {unique: true}
+      t.references :user, null: false, foreign_key: {to_table: :users}
+      t.timestamps
+    end
+  end
+end
